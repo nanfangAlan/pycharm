6939b87791367105607c1d4e0e88a8547ce3c379
nanfangalan <nanfangalan@gmail.com>
2018-07-04 15:14:50 +0800

    Initial commit

---
+++ mydict.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+' a test module '
+
+__author__ = 'Alan Calcifer'
+
+class Dict(dict):
+
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
---
+++ testtofind.py
@@ -0,0 +1,7 @@
+import re
+
+test = '用户输入的字符串'
+if re.match(r'正则表达式', test):
+    print('ok')
+else:
+    print('failed')
